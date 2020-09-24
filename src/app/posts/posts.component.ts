0b9b24dd49846c1af5d5fcfce164f0f586ad7598
junayed <junayed@intellier.com>
2020-09-24 20:37:10 +0600

    updated

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,11 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-
+    posts=[
+        {
+          title:'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+          body : 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+          id: 1,
+          author: 'me'
+        },
+        {
+          title:'ea molestias quasi exercitationem repellat qui ipsa sit aut',
+          body : 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
+          id: 2,
+          author: 'he'
+        },
+        {
+          title:'dolorem eum magni eos aperiam quia',
+          body : 'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut',
+          id: 3,
+          author: 'we'
+        },
+        {
+          title:'nesciunt iure omnis dolorem tempora et accusantium',
+          body : "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas",
+          id: 4,
+          author: 'she'
+        },
+        {
+          title:'in quibusdam tempore odit est dolorem',
+          body : 'itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio',
+          id: 5,
+          author: 'me'
+        },
+        {
+          title:'voluptate et itaque vero tempora molestiae',
+          body : 'eveniet quo quis\nlaborum totam consequatur non dolor\nut et est repudiandae\nest voluptatem vel debitis et magnam',
+          id: 6,
+          author: 'we'
+        }
+   ]
+   overlay=false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+  formActive(){
+     console.log("overlay")
+     this.overlay=true
   }
 
 }
